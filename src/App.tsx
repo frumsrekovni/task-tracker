b3f338d9d0d23543df5d44aa504f8758ec0d3248
frumsrekovni <joshua.backstrom@gmail.com>
2022-06-10 17:05:05 +0200

    Switched to a typescript environment. Played around with adding typescript logic in the app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+  // You can write Typescript up here
+  const name:string = "Joshua"
+  const x:Boolean=false;
+  return (
+    <div className="App">
+      <h1>Hello {name}!</h1>
+      <h2>Ternary operator: {x ? 'ef' : 'sfd'}</h2>
+    </div>
+  );
+}
+
+export default App;
