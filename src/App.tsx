e15b9ad047b90cf0ac06635c0878f628a9021b23
frumsrekovni <joshua.backstrom@gmail.com>
2022-06-11 12:03:10 +0200

    Basic component using props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
+import Header from './components/Header.tsx'
 function App() {
   // You can write Typescript up here
   const name:string = "Joshua"
   const x:Boolean=false;
   return (
     <div className="App">
+      <Header title='Jello' />
       <h1>Hello {name}!</h1>
       <h2>Ternary operator: {x ? 'ef' : 'sfd'}</h2>
     </div>
