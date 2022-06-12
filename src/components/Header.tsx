e3b04ad03fe62ccfae3f04ee9a5faf2274ea25ca
frumsrekovni <joshua.backstrom@gmail.com>
2022-06-12 23:35:13 +0200

    playing around with CSS and proptypes. This mixing of ts, html and css in the same file goes against everything ive previously learned

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,27 @@
-const Header = (props) => {
+import PropTypes from 'prop-types'
+
+const Header = ({title}) => {
   return (
    <header>
-    <h1>{props.title}</h1>
+    <h1 style= {headerStyle}>{title}</h1>
    </header>
   )
+}
+
+// You can set default values in case it is not set in the main app.
+Header.defaultProps = {
+  title: "Title Not Set"
+}
+
+Header.propTypes = {
+  title: PropTypes.string,
+}
+
+// This is heresy. Mixing typescript, html and CSS like this.
+// how low have we fallen
+const headerStyle = {
+    color: 'green',
+    backgroundColor: 'red'
 }
 
 export default Header
