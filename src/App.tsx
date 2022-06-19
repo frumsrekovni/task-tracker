e02e31ebaeb663e65a9fb095b4379845ca3b9bf8
frumsrekovni <joshua.backstrom@gmail.com>
2022-06-19 23:34:37 +0200

    Anonymous or named functions?

--- src/App.tsx
+++ src/App.tsx
@@ -30,18 +30,33 @@ function App() {
     }
 ])
 
-// Remove task
-const deleteTask = (id:number) => {
+// Filter out a task
   // Filter travels through the array and creates a new array with all the elements that pass the implemented test
   // In this case the test is passed as long as the id is not equal to the id that is passed into the onDelete function.
   // Meaning that the Task that called onDelete sends its own id effectively removing itself from the tasks with this filter function
+const deleteTask = (id:number) => {
+  // Anonymous function
   setTasks(tasks.filter((task) => task.id !== id));
 }
+
+function deleteTask2(id:number){
+  // Named function
+  setTasks(tasks.filter((task) => task.id !== id));
+}
+
+// Putting if statements down here with different portions of
+// html in each case is the benefit of using React.
   return (
     <div className="App">
       <Header title='Jello' />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
       <h2>Ternary operator: {x ? 'ef' : 'sfd'}</h2>
+      {tasks.length > 0 ? 
+      (
+      <Tasks tasks={tasks} onDelete={deleteTask2}/>
+      ):
+      (
+      "No tasks set"
+      )}
     </div>
   );
 }
