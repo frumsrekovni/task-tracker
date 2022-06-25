3bbe763f591c6b79a450126a6b576de26c462f55
frumsrekovni <joshua.backstrom@gmail.com>
2022-06-26 00:03:22 +0200

    added onchange for all the forms

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -4,19 +4,20 @@ const Add = () => {
   // setText and setDay are examples of 
   const [text, setText] = useState("");
   const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
   return (
     <form className="task-form">
         <div className="form-comp">
             <label>Task</label>
-            <input type="text" placeholder="Add" value={text}/>
+            <input type="text" placeholder="Add" value={text} onChange={(e) => setText(e.target.value)}/>
         </div>
         <div className="form-comp">
             <label>Day and Time</label>
-            <input type="text" placeholder="Add Day and Time" />
+            <input type="text" placeholder="Add Day and Time" value={day} onChange={(e) => setDay(e.target.value)} />
         </div>
         <div className="form-comp">
             <label>Remind?</label>
-            <input type="checkbox"/>
+            <input type="checkbox" value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
         </div>
     </form>
 
